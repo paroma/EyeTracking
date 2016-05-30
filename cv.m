6c8794be85f83cca8bf16b9a0940949d1e689693
Paroma Varma <paroma@DN51t40a.SUNet>
2016-05-29 23:59:41 -0700

    script to crop and parse CAVE dataset

---
+++ cv.m
@@ -0,0 +1,22 @@
+for subj=1:56
+    folder_path = sprintf('/Users/paroma/Desktop/Columbia Gaze Data Set/%04d/', subj);
+    cd(folder_path);
+    filename_match = sprintf('%04d_2m_0P_*', subj);
+    
+    jpegFiles = dir(filename_match); 
+    numfiles = length(jpegFiles);
+    for k = 1:numfiles 
+        img_to_move =jpegFiles(k).name; 
+        source = strcat(folder_path,img_to_move);
+        destination = '/Users/paroma/Desktop/Dataset';
+        movefile(source, destination);
+    end
+    
+end
+%%
+files = dir('*.jpg');
+for file = files'
+    a = imread(file.name);
+    a_crop = rgb2gray(a(1460:2173, 1600:3645,:));
+    imwrite(a_crop,strcat('aa_',file.name));
+end
